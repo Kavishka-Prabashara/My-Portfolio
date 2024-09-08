5a1ca78a905dd9654e3a95a8e37a13541ce7d2e3
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-09-08 12:54:49 +0530

    Add routing structure and integrate Navbar, Footer, and main pages (Home, About, Services, Project, Skills, Contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from "./component/navBar";
+import Footer from "./component/footer";
+
+import Home from "./pages/home";
+import About from "./pages/about";
+import Project from "./pages/project";
+import Skills from "./pages/skills";
+import Services from "./pages/service";
+import Contact from "./pages/home";
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-    <div>
-        <p color={"#1E90FF"}>kkkk</p>
-    </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    return (
+        <div>
+            <Navbar/>
+            <Home/>
+            <About/>
+            <Services/>
+            <Project/>
+            <Skills/>
+            <Contact/>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App
