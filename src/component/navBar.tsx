9d6ae2c4aab14b5dd0dc4acc034128e7149be1ca
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-09-10 23:40:41 +0530

    Implement smooth scrolling with react-scroll

--- src/component/navBar.tsx
+++ src/component/navBar.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import {Link} from 'react-scroll';
+import { Link } from 'react-scroll';
 
 const NavBar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -8,13 +8,12 @@ const NavBar = () => {
         setIsOpen(!isOpen);
     };
 
-
     const closeMenu = () => {
         setIsOpen(false);
     };
+
     return (
         <div
-
             className="w-full h-1 absolute flex justify-between items-center text-black py-6 px-8 md:px-32 bg-amber-200 drop-shadow-md rounded-2xl"
         >
             <a href="#">
@@ -29,37 +28,32 @@ const NavBar = () => {
                     <li
                         className="p-3 hover:bg-amber-200 hover:text-amber-50 rounded-md transition-all cursor-pointer"
                     >
-                        <Link to="/" spy={true} smooth={true} offset={500} onClick={closeMenu}>Home</Link>
+                        <Link to="home" spy={true} smooth={true} offset={-70} onClick={closeMenu}>Home</Link>
                     </li>
                     <li
                         className="p-3 hover:bg-amber-200 hover:text-amber-50 rounded-md transition-all cursor-pointer"
                     >
-                        <Link to="about" spy={true} smooth={true} offset={500} onClick={closeMenu}>About</Link>
-
+                        <Link to="about" spy={true} smooth={true} offset={-70} onClick={closeMenu}>About</Link>
                     </li>
                     <li
                         className="p-3 hover:bg-amber-200 hover:text-amber-50 rounded-md transition-all cursor-pointer"
                     >
-                        <Link to="about" spy={true} smooth={true} offset={500} onClick={closeMenu}>Service</Link>
-
+                        <Link to="services" spy={true} smooth={true} offset={-70} onClick={closeMenu}>Service</Link>
                     </li>
                     <li
                         className="p-3 hover:bg-amber-200 hover:text-amber-50 rounded-md transition-all cursor-pointer"
                     >
-                        <Link to="skills" spy={true} smooth={true} offset={500} onClick={closeMenu}>Skills</Link>
-
+                        <Link to="skills" spy={true} smooth={true} offset={-70} onClick={closeMenu}>Skills</Link>
                     </li>
                     <li
                         className="p-3 hover:bg-amber-200 hover:text-amber-50 rounded-md transition-all cursor-pointer"
                     >
-                        <Link to="projects" spy={true} smooth={true} offset={500} onClick={closeMenu}>Project</Link>
-
+                        <Link to="projects" spy={true} smooth={true} offset={-70} onClick={closeMenu}>Projects</Link>
                     </li>
                     <li
                         className="p-3 hover:bg-amber-200 hover:text-amber-50 rounded-md transition-all cursor-pointer"
                     >
-                        <Link to="contactMe" spy={true} smooth={true} offset={500} onClick={closeMenu}>Contact Me</Link>
-
+                        <Link to="contact" spy={true} smooth={true} offset={-70} onClick={closeMenu}>Contact Me</Link>
                     </li>
                 </ul>
                 <div className="relative hidden md:flex items-center justify-center gap-3 font-semibold text-base">
