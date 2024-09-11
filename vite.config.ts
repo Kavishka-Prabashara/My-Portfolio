1267dc00fdd7b1d1d998bcc9777a1fae97d39fd7
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-09-12 00:59:44 +0530

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/My-Portfolio/', // Correct base path for GitHub Pages deployment
-      build: {
-        outDir: 'build',
-      },
-      assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif']
-},
-    );
+        plugins: [react()],
+        base: '/My-Portfolio/', // Correct base path for GitHub Pages deployment
+        build: {
+            outDir: 'build',
+        },
+        assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif']
+    },
+);
