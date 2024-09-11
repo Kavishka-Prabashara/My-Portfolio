9cf6f6cc9415450cd23dca538abb0a3aad18a856
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-09-11 22:54:25 +0530

    Fixing gitHub host page blank screen bug

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/My-Portfolio/', // Correct base path for GitHub Pages deployment
-});
+      build: {
+        outDir: 'build',
+      },
+      assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif']
+},
+    );
