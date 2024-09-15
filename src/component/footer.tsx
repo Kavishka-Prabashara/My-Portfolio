36570c149d97227d22a3dc2fbcf73b84637f91f0
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-09-15 13:37:34 +0530

    update navbar url

--- src/component/footer.tsx
+++ src/component/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="bg-gray-800 text-white py-4">
             <div className="container mx-auto text-center">
                 <p className="text-sm">&copy; {new Date().getFullYear()} All Rights Reserved. Developed by <a
-                    href="https://www.linkedin.com/in/your-linkedin-url"
+                    href="https://www.linkedin.com/in/kavishka-prabashara-wijerathna-899588189/"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-blue-400 hover:underline">
